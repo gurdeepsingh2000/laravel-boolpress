fdd85bca2b7df692180ed64d39220de3a68c6e72
Gurdeep Singh <gurdeepsingh2000329@gmail.com>
2021-09-13 17:54:42 +0200

    aggiunta autenticazione

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,8 +7,29 @@ use App\Post;
 
 class HomeController extends Controller
 {
-    public function index(){
-        $allPosts = Post::all();
-        return view('HomePage',compact('allPosts'));
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
     }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index(){
+    // $User = Auth::user();
+
+    // if(empty($User)){
+    //     'You need to sign in';
+    // }
+
+        $allPosts = Post::all();
+        return view('home',compact('allPosts'));
+}
 }
---
+++ resources/views/home.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+    <div class="container">
+        <div class="row justify-content-center text-center">
+            @foreach($allPosts as $post)
+            <div class="post-card py-5">
+                <div class="post-bar">Luogo: {{$post->Place}}</div>
+
+                <div class="post-user">
+                    <span>Nome: {{$post->name}}</span>
+                    <span>Cognome: {{$post->surname}}</span>
+                </div>
+
+                    <div class="post-cover">
+                        <div class="img-container">
+                            <img src="{{$post->cover}}" alt="">
+                        </div>
+                    </div>
+
+                <div class="user-interaction">
+                    <div class="number-likes"><span>Likes </span>{{$post->likes}}</div>
+                    <div class="number-comments"><span>Commenti </span>{{$post->comments}}</div>
+                </div>
+         </div>
+@endforeach
+
+    </div>
+</div>
+
+@endsection
